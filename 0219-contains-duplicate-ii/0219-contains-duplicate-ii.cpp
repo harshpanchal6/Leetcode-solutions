552ba6b07daa2bb2fa82b92fc09214d89de26bee
harshpanchal6 <harshpanchal5244@gmail.com>
2026-09-10 01:25:53 +0530

    Time: 111 ms (49.67%), Space: 142.2 MB (9.1%) - LeetHub

---
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool containsNearbyDuplicate(vector<int>& nums, int k) {
+        
+        int n=nums.size();
+        unordered_map<int,int>hash;
+
+        for(int i=0;i<n;i++){
+            if(hash.find(nums[i])!=hash.end()){
+                if(i-hash[nums[i]]<=k){
+                    return true;
+                }
+            }
+            hash[nums[i]]=i;
+        }
+        return false;
+    }
+};
