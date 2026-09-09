bacf32b81ac54b3941d8a98887db90dac2fb32e7
harshpanchal6 <harshpanchal5244@gmail.com>
2026-09-10 01:40:53 +0530

    Time: 1 ms (24.9%), Space: 22.6 MB (5.14%) - LeetHub

---
+++ 0268-missing-number/0268-missing-number.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> hash(n + 1, 0);
+        
+        for(int i : nums){
+            hash[i]++;
+        }
+        
+        for(int i = 0; i < n + 1; i++){
+            if(hash[i] == 0){
+                return i;
+            }
+        }
+        
+        return -1; 
+    }
+};
